4f90406b26f88f9fe11830be382b579f7b081dee
Isaac Garcia <isaacmg123@gmail.com>
2022-09-25 02:05:53 -0700

    found affected packages installed on system

--- main.py
+++ main.py
@@ -19,12 +19,35 @@ for i in arch_packages:
 
 # Python 3.5+
 # cmd for arch based systems pacman -Q
-cmd = 'pacman -Q'
+cmd = 'pacman -Q '
+cmd2 = 'cut -f 1 -d " "'
 args = shlex.split(cmd)
+args2 = shlex.split(cmd2)
+
 proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-output = proc.stdout.read()
+output = subprocess.check_output(args2, stdin=proc.stdout)
+#output = proc.stdout.read(args2, stdin=proc.stdout)
+proc.wait()
+
 output = output.decode().splitlines()
-print(output)
+for item in output:
+    print(item)
 print(type(output))
+print(type(arch_packages))
 
 print(len(output))
+
+check = any(item in arch_packages for item in output)
+
+print(list(set(arch_packages) & set(output)))
+print(check)
+
+
+def intersection(lst1, lst2):
+
+    temp = set(lst2)
+    lst3 = [value for value in lst1 if value in temp]
+    return lst3
+
+
+print(intersection(arch_packages, output))
