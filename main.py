db2cd70ca60e5f3bab5a7170517621238fe2e3d0
Isaac <43230315+isaacmg00@users.noreply.github.com>
2022-11-15 01:59:37 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import distro
 from bs4 import BeautifulSoup
 
 
+class Package:
+    def __init__(self, group, issue, package, affected, fixed, severity, status, ticket, advisory):
+        self.group = group
+        self.issue = issue
+        self.package = package
+        self.affected = affected
+        self.fixed = fixed
+        self.severity = severity
+        self.status = status
+        self.ticket = ticket
+        self.advisory = advisory
+
+
 def get_system_distribution():
     return subprocess.check_output(['./get_distro.sh'])
 
@@ -23,12 +36,33 @@ s = soup.find_all('td', class_='wrap')
 arch_packages = []
 
 for x in soup.find_all('td', class_='wrap'):
-    arch_packages.append(x.text[1:-1])
+    arch_packages.append(x.text)
+    # arch_packages.append(x.text[1:-1])
 
+text = []
+
+for i in soup.find_all('tr'):
+    text.append(i.text)
+
+print(text[1])
+
+string = text[1].split("\n")
+for item in string:
+    if(item == ''):
+        print("found nothing")
+
+
+print(string)
+print(type(text[1]))
+
+
+'''
+for i in range(1, len(text)):
+    group = i
+    package = Package
+    print(text[i])
+'''
 print(arch_packages)
-for i in arch_packages:
-    print(i)
-
 # Python 3.5+
 # cmd for arch based systems pacman -Q
 cmd = 'pacman -Q '
@@ -41,12 +75,6 @@ output = subprocess.check_output(args2, stdin=proc.stdout)
 proc.wait()
 
 output = output.decode().splitlines()
-for item in output:
-    print(item)
-print(type(output))
-print(type(arch_packages))
-
-print(len(output))
 
 check = any(item in arch_packages for item in output)
 
@@ -71,5 +99,5 @@ print("found " + str(len(vulnerable_packages)) +
       " vulnerable packages on local system.")
 print("Check for updates? y/n")
 
-
 print(get_package_manager(system_distro))
+
