41f397a9ef02318c25c652f6fe0d0f99af144d02
Isaac Garcia <isaacmg123@gmail.com>
2023-03-23 01:12:07 -0700

    print out severity status

--- main.py
+++ main.py
@@ -5,19 +5,6 @@ import distro
 from bs4 import BeautifulSoup
 
 
-class Package:
-    def __init__(self, group, issue, package, affected, fixed, severity, status, ticket, advisory):
-        self.group = group
-        self.issue = issue
-        self.package = package
-        self.affected = affected
-        self.fixed = fixed
-        self.severity = severity
-        self.status = status
-        self.ticket = ticket
-        self.advisory = advisory
-
-
 def get_system_distribution():
     return subprocess.check_output(['./get_distro.sh'])
 
@@ -31,51 +18,17 @@ def get_package_manager(distro):
 
 r = requests.get('https://security.archlinux.org/')
 soup = BeautifulSoup(r.content, 'html.parser')
-s = soup.find_all('td', class_='wrap')
-
 arch_packages = []
+vuln_packages = []
 
-tables = soup.findChildren('table')
-my_table = tables[0]
-rows = my_table.findChildren(['th', 'tr'])
+index = 1
+list = []
 
-for row in rows:
-    cells = row.findChildren('td')
-    for cell in cells:
-        value = cell.text
-        print(value)
-'''
 for x in soup.find_all('td', class_='wrap'):
-    arch_packages.append(x.text)
-    # arch_packages.append(x.text[1:-1])
+    list.append([x.text[1:-1], index])
+    index += 1
+    arch_packages.append(x.text[1:-1])
 
-text = []
-
-for i in soup.find_all('tr'):
-    text.append(i.text)
-
-print(text[11])
-
-string = text[11].split("\n")
-
-for item in string:
-    if(item == ''):
-        print("found nothing")
-
-
-print(string)
-print(type(text[1]))
-
-
-'''
-'''
-for i in range(1, len(text)):
-    group = i
-    package = Package
-    print(text[i])
-'''
-'''
-print(arch_packages)
 # Python 3.5+
 # cmd for arch based systems pacman -Q
 cmd = 'pacman -Q '
@@ -89,21 +42,17 @@ proc.wait()
 
 output = output.decode().splitlines()
 
-check = any(item in arch_packages for item in output)
-
-print(list(set(arch_packages) & set(output)))
-print(check)
-
 
 def intersection(lst1, lst2):
-
     temp = set(lst2)
     lst3 = [value for value in lst1 if value in temp]
     return lst3
 
 
-print(intersection(arch_packages, output))
-vulnerable_packages = intersection(arch_packages, output)
+vulnerable_packages = set(intersection(arch_packages, output))
+
+for vuln in vulnerable_packages:
+    print(str(arch_packages.index(vuln)+1) + " " + str(vuln))
 
 
 num_packages = len(output)
@@ -113,4 +62,3 @@ print("found " + str(len(vulnerable_packages)) +
 print("Check for updates? y/n")
 
 print(get_package_manager(system_distro))
-'''
