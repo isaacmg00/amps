30ee5755527a1afe72d6431663180dad06041f95
Isaac Garcia <isaacmg123@gmail.com>
2023-03-28 01:50:56 -0700

    finalizing output changes

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import subprocess
 import shlex
 import distro
 from bs4 import BeautifulSoup
+from lxml import etree
+from tqdm import tqdm
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+table = Table(show_header=True, header_style="bold")
+table.add_column("Package", justify="left")
+table.add_column("Severity", justify="left")
+table.add_column("Status", justify="left")
+table.add_column("Issue", justify="left")
 
 
 def get_system_distribution():
@@ -24,10 +36,10 @@ vuln_packages = []
 index = 1
 list = []
 
-for x in soup.find_all('td', class_='wrap'):
+for x in tqdm(soup.find_all('td', class_='wrap'), desc="finding vulnerable packages", ascii=" ####", bar_format="{desc}: {percentage:.1f}%[{bar}]"):
     list.append([x.text[1:-1], index])
     index += 1
-    arch_packages.append(x.text[1:-1])
+    arch_packages.append(x.text[1: -1])
 
 # Python 3.5+
 # cmd for arch based systems pacman -Q
@@ -36,7 +48,8 @@ cmd2 = 'cut -f 1 -d " "'
 args = shlex.split(cmd)
 args2 = shlex.split(cmd2)
 
-proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+proc = subprocess.Popen(args, stdout=subprocess.PIPE,
+                        stderr=subprocess.STDOUT)
 output = subprocess.check_output(args2, stdin=proc.stdout)
 proc.wait()
 
@@ -51,14 +64,28 @@ def intersection(lst1, lst2):
 
 vulnerable_packages = set(intersection(arch_packages, output))
 
-for vuln in vulnerable_packages:
-    print(str(arch_packages.index(vuln)+1) + " " + str(vuln))
+dom = etree.HTML(str(soup))
 
+for vuln in vulnerable_packages:
+    severity = dom.xpath('/ html/body/div[2]/div[3]/table/tbody/tr[' + str(
+        arch_packages.index(vuln)+1) + ']/td[6]/span')[0].text
+
+    status = dom.xpath('/ html/body/div[2]/div[3]/table/tbody/tr[' + str(
+        arch_packages.index(vuln)+1) + ']/td[7]/span')[0].text
+
+    cve = dom.xpath('/ html/body/div[2]/div[3]/table/tbody/tr[' + str(
+        arch_packages.index(vuln)+1) + ']/td[2]/a[1]')[0].text
+
+    issue = cve + " (https://security.archlinux.org/" + str(cve) + ")"
+
+    table.add_row(
+        vuln, severity, status, issue
+    )
 
 num_packages = len(output)
-print(str(num_packages) + " installed packages.")
+print(str(num_packages) + " packages installed via pacman.")
 print("found " + str(len(vulnerable_packages)) +
       " vulnerable packages on local system.")
-print("Check for updates? y/n")
+print("distribution: " + get_package_manager(system_distro))
 
-print(get_package_manager(system_distro))
+console.print(table)
